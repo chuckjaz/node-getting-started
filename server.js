016bf650ef72f807620d4a1915109c86fa4625ab
Chuck Jazdzewski <chuckj@google.com>
2015-01-30 16:21:13 -0800

    Using express

--- server.js
+++ server.js
@@ -1,6 +1,12 @@
-var http = require('http');
-http.createServer(function (req, res) {
-  res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Hello World\n');
-}).listen(8080, '0.0.0.0');
-console.log('Listening on 8080');
+var express = require('express');
+var app = express();
+
+// Add routes
+app.get("/", function(req, res) {
+  res.send('Hello World!\n');
+});
+
+// Start server
+var server = app.listen(8080, function () {
+  console.log('Server running at http://localhost:%s', server.address().port);
+});
