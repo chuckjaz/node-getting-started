bff8fc76cc093f15a594de57e7d325179aeb2d8c
Chuck Jazdzewski <chuckj@google.com>
2015-02-02 13:55:05 -0800

    Storing data in Datastore

--- server.js
+++ server.js
@@ -1,12 +1,27 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+var gcloud = require('gcloud');
 var app = express();
 
-// Book data
-var books = [
-    { name: "War and Peace", author: "Leo Tolstoy" },
-    { name: "The Count of Monte Cristo", author: "Alexandre Dumas" }
-  ];
+// Initialize the store
+var dataset = gcloud.datastore.dataset({ 
+  projectId: process.env.PROJECT_ID
+});
+
+// Add a book
+function addBook(book, cb) {
+  dataset.save({ key: dataset.key('Book'), data: book }, cb);
+}
+
+// Find all books
+function findAllBooks(cb) {
+  dataset.runQuery(dataset.createQuery(['Book']), cb);
+}
+
+// Find an author's books
+function findAuthorBooks(author, cb) {
+  dataset.runQuery(dataset.createQuery(['Book']).filter("author = ", author), cb);
+}
 
 // Add middle-ware
 app.use(bodyParser.json());
@@ -18,21 +33,32 @@ app.get("/", function(req, res) {
 
 // GET /books returns all books
 app.get("/books", function (req, res) {
+  findAllBooks(function (err, books) {
+    if (err) return res.sendStatus(500);
+    res.json(books.map(function (entity) {
+      return entity.data;
+    }));
+  });
   res.json(books);
 });
 
 // GET /books/Leo%20Tolstoy returns all books where author="Leo Tolstoy"
 app.get("/books/:author", function (req, res) {
-  res.json(books.filter(function (book) {
-    return book.author == req.params.author
-  }));
+  findAuthor(req.params.author, function (err, books) {
+    if (err) return res.sendStatus(500);
+    res.json(books.map(function (entity) {
+      return entity.data;
+    }));
+  });
 });
 
 // Create a book with a given name and author
 app.put("/books", function (req, res) {
   if (!req.body || !req.body.name || !req.body.author) return res.sendStatus(400);
-  books.push({ name: req.body.name, author: req.body.author });
-  res.sendStatus(200);
+  addBook({ author: req.body.author, name: req.body.name }, function (err) {
+    if (err) return res.sendStatus(500);
+    res.sendStatus(200);
+  });
 });
 
 // Start server
