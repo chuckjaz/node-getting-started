af7b8fd26863130f4ed28d70bad9fad1b90501ed
Chuck Jazdzewski <chuckj@google.com>
2015-02-02 13:30:08 -0800

    Adding a simple REST GET handler

--- server.js
+++ server.js
@@ -1,9 +1,20 @@
 var express = require('express');
 var app = express();
 
+// Book data
+var books = [
+    { name: "War and Peace", author: "Leo Tolstoy" },
+    { name: "The Count of Monte Cristo", author: "Alexandre Dumas" }
+  ];
+
 // Add routes
 app.get("/", function(req, res) {
   res.send('Hello World!\n');
+});
+
+// GET /books returns all books
+app.get("/books", function (req, res) {
+  res.json(books);
 });
 
 // Start server
