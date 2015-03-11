2c342819ea3570bdb30fc37bd509089f29767108
Chuck Jazdzewski <chuckj@google.com>
2015-03-11 16:25:02 -0700

    Added saving and retrieving data from Datastore

--- server.js
+++ server.js
@@ -2,10 +2,26 @@ var express = require('express'),
     bodyParser = require('body-parser'),
     exhbrs = require('express-handlebars');
 
-var books = [
-    { title: "War and Peace", author: "Leo Tolstoy" },
-    { title: "The Count of Monte Cristo", author: "Alexandre Dumas" }
-  ];
+var gcloud = require('gcloud');
+
+// Initialize the store
+var dataset = gcloud.datastore.dataset({
+  projectId: 'chuckj-node-scratch',
+  credentials: require('./keyfile.json')
+});
+
+// Add a book
+function addBook(book, cb) {
+  dataset.save({ key: dataset.key('Book'), data: book }, cb);
+}
+
+// Find all books
+function findAllBooks(cb) {
+  dataset.runQuery(dataset.createQuery(['Book']), function (err, books) {
+    if (err) cb(err)
+    cb(null, books.map(function(item) { return item.data; }));
+  });
+}
 
 var app = express();
 
@@ -18,12 +34,17 @@ app.engine('handlebars', exhbrs({ defaultLayout: 'main'}));
 app.set('view engine', 'handlebars');
 
 app.get('/', function (req, res) {
-  res.render('main', { books: books });
+  findAllBooks(function (err, books) {
+    if (err) return res.sendStatus(500);
+    res.render('main', { books: books });
+  })
 })
 
 app.post('/add', function (req, res) {
-  books.push({ author: req.body.author, title: req.body.title })
-  res.redirect('/')
+  addBook({ author: req.body.author, title: req.body.title }, function (err) {
+    if (err) return res.sendStatus(500);
+    res.redirect('/')
+  })
 })
 
 app.listen(8080);
